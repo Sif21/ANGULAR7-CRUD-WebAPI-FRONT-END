c2a3e8c049f58319c90348d6a31aa62c916249b2
Andrew <andrew@prueba.com>
2019-01-08 04:06:29 -0500

    CRUD FINALIZADO

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/employee.service';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: EmployeeService,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  resetForm(form? : NgForm){
+    if (form != null)
+       form.resetForm();
+    this.service.formData = {
+      EmployeeID : null,
+      FullName : '',
+      Position : '',
+      EMPCode : '',
+      Mobile : ''
+    }
+  }
+
+  onSubmit(form: NgForm){
+    if(form.value.EmployeeID==null)
+    this.insertRecord(form);
+    else
+    this.updateRecord(form);
+  }
+
+  insertRecord(form: NgForm){
+    this.service.postEmployee(form.value).subscribe(res => {
+      this.toastr.success('Empleado nuevo','EMP. Registrado');
+      this.resetForm(form);
+      this.service.refreshList();
+    });
+  }
+
+  updateRecord(form: NgForm){
+    this.service.putEmployee(form.value).subscribe(res => {
+      this.toastr.info('Empleado actualizado','EMP. Actualizado');
+      this.resetForm(form);
+      this.service.refreshList();
+    });
+  }
 }
